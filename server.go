a1b1057cd22e1c7ffa149c95e27ed8e5834110bd
Zoltan Farkas <progfarkas@gmail.com>
2024-10-01 00:05:06 +0200

    GoP PoC

--- server.go
+++ server.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"github.com/asticode/go-astits"
+	"github.com/bluenviron/mediacommon/pkg/codecs/h264"
+	"github.com/bluenviron/mediacommon/pkg/codecs/h265"
+	"github.com/bluenviron/mediacommon/pkg/formats/mpegts"
 	srt "github.com/datarhei/gosrt"
-	"io"
 	"log"
-	"os/exec"
+	"os"
+	"strconv"
+	"time"
 )
 
 func testServer() {
@@ -62,45 +69,82 @@ func isPublish(req srt.ConnRequest) bool {
 }
 
 func handlePublish(conn srt.Conn) {
-	cmd := exec.Command("ffplay", "-i", "pipe:0", "-probesize", "32", "-analyzeduration", "0")
-	ffplayIn, err := cmd.StdinPipe()
-	if err != nil {
-		log.Fatalf("Failed to create stdin pipe: %v", err)
-	}
-	defer ffplayIn.Close()
-
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Fatalf("Failed to get stdout: %v", err)
-	}
-
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		log.Fatalf("Failed to get stderr: %v", err)
-	}
-
-	err = cmd.Start()
-	if err != nil {
-		log.Fatalf("Failed to start ffplay: %v", err)
-	}
-
-	go io.Copy(log.Writer(), stdout)
-	go io.Copy(log.Writer(), stderr)
-
-	defer cmd.Cancel()
-
-	buffer := make([]byte, bufferSize)
+	demuxer := astits.NewDemuxer(context.Background(), mpegts.NewBufferedReader(conn))
+	var muxer *mpegtsMuxer
 	for {
-		n, err := conn.Read(buffer)
+		data, err := demuxer.NextData()
 		if err != nil {
-			log.Printf("Read error: %v", err)
+			log.Println(err)
 			break
 		}
-
-		_, err = ffplayIn.Write(buffer[:n])
-		if err != nil {
-			log.Printf("Write error: %v", err)
-			break
+		if data.PES == nil {
+			continue
+		}
+		au, ue := h264.AnnexBUnmarshal(data.PES.Data)
+		if ue != nil {
+			log.Println(ue)
+			continue
+		}
+		var ntp time.Time
+		hasNtp := false
+		if data.PES.Header.OptionalHeader != nil && data.PES.Header.OptionalHeader.HasPrivateData {
+			privateData := data.PES.Header.OptionalHeader.PrivateData
+			var timestamp int64
+			for i := uint(0); i < 8; i++ {
+				timestamp |= int64(privateData[i+1]) << (8 * (7 - i))
+			}
+			ntp = time.UnixMilli(timestamp)
+			hasNtp = true
+			log.Printf("Time: %s", ntp)
+		}
+		if data.FirstPacket != nil && data.FirstPacket.AdaptationField != nil && data.FirstPacket.AdaptationField.RandomAccessIndicator {
+			if muxer != nil {
+				muxer.close()
+			}
+			var vps []byte
+			var sps []byte
+			var pps []byte
+			for _, nalu := range au {
+				typ := h265.NALUType((nalu[0] >> 1) & 0b111111)
+				switch typ {
+				case h265.NALUType_VPS_NUT:
+					vps = nalu
+				case h265.NALUType_SPS_NUT:
+					sps = nalu
+				case h265.NALUType_PPS_NUT:
+					pps = nalu
+				}
+			}
+			var tstime = ntp
+			if !hasNtp {
+				log.Println("Use system time")
+				tstime = time.Now()
+			}
+			if len(vps) > 0 && len(sps) > 0 && len(pps) > 0 {
+				file, err := os.Create("output-" + strconv.FormatInt(tstime.UnixMilli(), 10) + ".ts")
+				if err != nil {
+					log.Fatalf("Failed to create file: %v", err)
+				}
+				muxer = &mpegtsMuxer{
+					vps: vps,
+					sps: sps,
+					pps: pps,
+					b:   bufio.NewWriterSize(file, bufferSize),
+				}
+				err = muxer.initialize()
+				if err != nil {
+					panic(err)
+				}
+			} else {
+				panic("missing codec params")
+			}
+		}
+		if muxer != nil && data.PES != nil && data.PES.Header.OptionalHeader != nil && data.PES.Header.OptionalHeader.PTS != nil && data.PES.Header.IsVideoStream() {
+			pts := data.PES.Header.OptionalHeader.PTS.Duration()
+			werr := muxer.writeH265(au, pts, ntp, hasNtp) // TODO: use a simple muxer that uses the received DTS and does not create private data with timestamp
+			if werr != nil {
+				log.Println(werr)
+			}
 		}
 	}
 }
